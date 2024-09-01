01097a0873d24dc155a033030c75b0a0354c7d70
Jack O'Connor <j.oconnor@yale.edu>
2024-09-01 15:11:46 -0400

    updated schema (v2) to allow for MatchupWeek for conveninece

--- sleeper.sql
+++ sleeper.sql
@@ -1,47 +1,23 @@
+DROP TABLE IF EXISTS "User";
+DROP TABLE IF EXISTS "Player";
+DROP TABLE IF EXISTS "League";
+DROP TABLE IF EXISTS "LeagueUser";
+DROP TABLE IF EXISTS "LeagueRoster";
+DROP TABLE IF EXISTS "RosterPlayer";
+DROP TABLE IF EXISTS "MatchupWeek";
+DROP TABLE IF EXISTS "MatchupRoster";
+DROP TABLE IF EXISTS "MatchupRosterPlayer";
+
+
 CREATE TABLE "User" (
-  "UserID" TEXT,
+  "UserID" TEXT PRIMARY KEY,
   "UserName" TEXT,
   "DisplayName" TEXT,
   "JSONData" TEXT
 );
 
-CREATE TABLE "League" (
-  "LeagueID" TEXT,
-  "Season" varchar(4),
-  "JSONData" TEXT,
-  "Previous_League_ID" TEXT,
-  "Name" TEXT,
-  "DraftID" INTEGER
-);
-
-CREATE TABLE "LeagueUser" (
-  "LeagueUserID" INTEGER,
-  "UserID" TEXT,
-  "LeagueID" TEXT,
-  "TeamName" TEXT,
-  "AvatarID" INTEGER
-);
-
-CREATE TABLE "Roster" (
-  "RosterID" INTEGER,
-  "OwnerID" TEXT,
-  "Record" TEXT,
-  "Streak" TEXT,
-  "Fpts" int,
-  "FptsAgainst" int,
-  "JSONData" TEXT
-);
-
-CREATE TABLE "RosterPlayer" (
-  "RosterPlayerID" INTEGER,
-  "RosterID" INTEGER,
-  "PlayerID" TEXT,
-  "Position" TEXT,
-  "Starter" bit
-);
-
 CREATE TABLE "Player" (
-  "PlayerID" TEXT,
+  "PlayerID" TEXT PRIMARY KEY,
   "FirstName" TEXT,
   "LastName" TEXT,
   "Team" TEXT,
@@ -50,25 +26,73 @@ CREATE TABLE "Player" (
   "JSONData" TEXT
 );
 
-CREATE TABLE "Matchup" (
-  "MatchupID" INTEGER,
-  "SleeperMatchupID" TEXT,
+CREATE TABLE "League" (
+  "LeagueID" TEXT PRIMARY KEY,
+  "Season" TEXT,
+  "JSONData" TEXT,
+  "Previous_League_ID" TEXT,
+  "Name" TEXT,
+  "DraftID" INTEGER
+);
+
+CREATE TABLE "LeagueUser" (
+  "LeagueUserID" INTEGER PRIMARY KEY,
+  "UserID" TEXT,
   "LeagueID" TEXT,
-  "RosterID" TEXT,
-  "Week" INTEGER
+  "TeamName" TEXT,
+  "AvatarID" INTEGER,
+  FOREIGN KEY ("UserID") REFERENCES "[User].UserID",
+  FOREIGN KEY ("LeagueID") REFERENCES "League.LeagueID"
+);
+
+CREATE TABLE "LeagueRoster" (
+  "LeagueRosterID" INTEGER PRIMARY KEY,
+  "RosterCode" INTEGER,
+  "OwnerID" TEXT,
+  "LeagueID" TEXT,
+  "Record" TEXT,
+  "Streak" TEXT,
+  "Fpts" DECIMAL,
+  "FptsAgainst" DECIMAL,
+  "JSONData" TEXT,
+  FOREIGN KEY ("OwnerID") REFERENCES "[User].UserID",
+  FOREIGN KEY ("LeagueID") REFERENCES "League.LeagueID",
+  CONSTRAINT unique_owner UNIQUE ("OwnerID", "LeagueID")
+);
+
+CREATE TABLE "RosterPlayer" (
+  "RosterPlayerID" INTEGER PRIMARY KEY,
+  "LeagueRosterID" INTEGER,
+  "PlayerID" TEXT,
+  "Position" TEXT,
+  "Starter" bit,
+  FOREIGN KEY ("LeagueRosterID") REFERENCES "LeagueRoster.LeagueRosterID",
+  FOREIGN KEY ("PlayerID") REFERENCES "Player.PlayerID",
+  CONSTRAINT unique_roster_player UNIQUE ("LeagueRosterID", "PlayerID")
+);
+
+CREATE TABLE "MatchupWeek" (
+  "MatchupWeekID" INTEGER PRIMARY KEY,
+  "MatchupCode" INTEGER,
+  "Week" INTEGER,
+  "LeagueID" TEXT,
+  CONSTRAINT unique_matchup UNIQUE ("MatchupCode", "Week", "LeagueID")
 );
 
 CREATE TABLE "MatchupRoster" (
-  "MatchupRosterID" INTEGER,
-  "MatchupID" INTEGER,
-  "RosterID" INTEGER,
-  "Points" INTEGER
+  "MatchupRosterID" INTEGER PRIMARY KEY,
+  "MatchupWeekID" INTEGER,
+  "OwnerRosterCode" INTEGER,
+  "Points" DECIMAL,
+  FOREIGN KEY ("MatchupWeekID") REFERENCES "MatchupWeek.MatchupWeekID",
+  CONSTRAINT unique_owner UNIQUE ("MatchupWeekID", "OwnerRosterCode")
 );
 
 CREATE TABLE "MatchupRosterPlayer" (
+  "MatchupRosterPlayerID" INTEGER PRIMARY KEY,
   "MatchupRosterID" INTEGER,
   "PlayerID" TEXT,
-  "Starter" bit,
-  "Points" bit
+  "Starter" BIT,
+  "Points" DECIMAL,
+  FOREIGN KEY ("MatchupRosterID") REFERENCES "MatchupRoster.MatchupRosterID"
 );
-
