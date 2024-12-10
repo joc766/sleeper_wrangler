e7902595bf9166df161f98111022b70f4b73301e
Jack O'Connor <j.oconnor@yale.edu>
2024-12-09 23:24:24 -0500

    refactoring, no functional changes

--- get_data.py
+++ get_data.py
@@ -1,19 +1,15 @@
 import sqlite3
 import json
-
 from sleeper_api import get_league, get_league_users, get_matchups, get_rosters, get_user_data
 
-LEAGUE_ID = '868563615295410176'
-# Old to New:
-# 868563615295410176
-# 990267272524541952
-# 1120774194318479360
+def process_league(league_id, conn):
+    """
+    Processes and inserts all data related to a single league into the database.
+    """
+    cursor = conn.cursor()
 
-if __name__ == "__main__":
-    #############################
     # LEAGUE
-    #############################
-    league = get_league(LEAGUE_ID)
+    league = get_league(league_id)
     league_qry = '''
         INSERT INTO League (LeagueID, Season, JSONData, Previous_League_ID, Name, DraftID)
         VALUES (?, ?, ?, ?, ?, ?)
@@ -21,16 +17,15 @@ if __name__ == "__main__":
     league_data = (
         league["league_id"],
         league["season"],
-        json.dumps(league),  # JSON data, safely handled
+        json.dumps(league),
         league["previous_league_id"],
         league["name"],
-        league["draft_id"]
+        league["draft_id"],
     )
+    cursor.execute(league_qry, league_data)
 
-    #############################
     # USERS
-    #############################
-    league_users = get_league_users(LEAGUE_ID)
+    league_users = get_league_users(league_id)
     team_data = {}
     user_qry = '''
         INSERT OR IGNORE INTO [User] (UserID, UserName, DisplayName, JSONData)
@@ -38,86 +33,74 @@ if __name__ == "__main__":
     '''
     users_data = []
     for data in league_users:
-        if not data.get("metadata") or not data["metadata"].get("team_name"): 
-            team_name = ''
-        else:
-            team_name = data["metadata"]["team_name"]
-
+        team_name = data.get("metadata", {}).get("team_name", "")
         team_data[data["user_id"]] = team_name
 
         user_data = get_user_data(data["user_id"])
-
         users_data.append((
             user_data["user_id"],
             user_data["username"],
             user_data["display_name"],
-            json.dumps(user_data)
+            json.dumps(user_data),
         ))
+    cursor.executemany(user_qry, users_data)
 
-
-    #############################
     # TEAMS
-    #############################
-    teams_data = []
-    rosters = get_rosters(LEAGUE_ID)
+    rosters = get_rosters(league_id)
     teams_qry = '''
         INSERT INTO Team (UserID, RosterCode, LeagueID, TeamName, Record, Streak, Fpts, FptsAgainst)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?)
     '''
-    # roster_player_qry = 'INSERT INTO RosterPlayer (RosterPlayerID, RosterID, PlayerID, Starter) VALUES '
-    for i, data in enumerate(rosters):
-        if not data.get('owner_id'):
-            team_name = 'unknown'
-            data['owner_id'] = None
-        else:
-            team_name = team_data[data['owner_id']]
-
+    teams_data = []
+    for data in rosters:
+        team_name = team_data.get(data.get('owner_id'), 'unknown')
         teams_data.append((
-            data["owner_id"],
+            data.get("owner_id"),
             data["roster_id"],
-            LEAGUE_ID,
+            league_id,
             team_name,
-            data["metadata"].get("record"),
-            data["metadata"].get("streak"),
-            data["settings"].get("fpts"),
-            data["settings"].get("fpts_against")
+            data.get("metadata", {}).get("record"),
+            data.get("metadata", {}).get("streak"),
+            data.get("settings", {}).get("fpts"),
+            data.get("settings", {}).get("fpts_against"),
         ))
-    #     starters = set(data['starters'])
-    #     for j, player_id in enumerate(data['players']):
-    #         roster_player_qry += f'({i*len(data["players"])+j},\'{data["roster_id"]}\',\'{player_id}\',{1 if player_id in starters else 0})'
-    #         if i != len(rosters) - 1 or j != len(data['players']) - 1:
-    #             roster_player_qry += ','
+    cursor.executemany(teams_qry, teams_data)
 
-
-    #############################
     # MATCHUPS
-    #############################
-    matchups = get_matchups(LEAGUE_ID)
-
+    matchups = get_matchups(league_id)
     matchup_roster_qry = '''
         INSERT INTO MatchupRoster (LeagueID, MatchupCode, RosterCode, Week, Points)
         VALUES (?, ?, ?, ?, ?)
     '''
     matchups_data = []
     for data in matchups:
-        matchup_id = data["matchup_id"] if data["matchup_id"] is not None else None
         matchups_data.append((
-            LEAGUE_ID,
-            matchup_id,
+            league_id,
+            data.get("matchup_id"),
             data["roster_id"],
             data["week"],
-            data["points"]
+            data["points"],
         ))
+    cursor.executemany(matchup_roster_qry, matchups_data)
+
+    conn.commit()
+    return league["previous_league_id"]
 
 
-    with sqlite3.connect('/Users/jackoconnor/Desktop/Football/sleeper.db') as conn:
-        cursor = conn.cursor()
-        try:
-            # cursor.execute(player_qry)
-            cursor.execute(league_qry, league_data)
-            cursor.executemany(user_qry, users_data)
-            cursor.executemany(teams_qry, teams_data)
-            cursor.executemany(matchup_roster_qry, matchups_data)
-            conn.commit()
-        finally:
-            cursor.close()
+def main():
+    # List of league IDs to process
+    league_ids = ['1120774194318479360', '868563615295410176', '990267272524541952'] # new to old, here for reference
+    latest_league_id = league_ids[0] 
+
+    # Database path
+    db_path = '/Users/jackoconnor/Desktop/Football/sleeper.db'
+
+    # Connect to the database and process each league
+    with sqlite3.connect(db_path) as conn:
+        curr_league = latest_league_id
+        while curr_league is not None:
+            print(f"Processing league: {curr_league}")
+            curr_league = process_league(curr_league, conn)
+
+if __name__ == "__main__":
+    main()
