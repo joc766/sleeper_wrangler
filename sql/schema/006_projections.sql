08598880160b0cbf8ab8f4fedf11c787aff56819
Jack <razor-hautes01@icloud.com>
2026-09-22 10:00:25 -0400

    add uniqueness constraint to Projections

--- sql/schema/006_projections.sql
+++ sql/schema/006_projections.sql
@@ -7,6 +7,8 @@ create table Projections (
   PlayerID TEXT NOT NULL,
   InjuryStatus TEXT NULL,
   PointsHalfPPR REAL, -- ok with floating point for now, could convert to hundredths of pts
+  
+  UNIQUE(Season, Week, PlayerID),
   FOREIGN KEY (PlayerID) REFERENCES Player (PlayerID)
 );
 
