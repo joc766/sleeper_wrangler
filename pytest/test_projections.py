27805a3ce813e5c005deef34397d0ddd4388508c
Jack <razor-hautes01@icloud.com>
2026-09-23 10:43:39 -0400

    Add quotes to season in test_get_projections

--- pytest/test_projections.py
+++ pytest/test_projections.py
@@ -6,7 +6,7 @@ from sleeper_wrangler.sleeper_api import get_projections
 
 @pytest.mark.integration
 def test_get_projections():
-    result = get_projections(2026, 1)
+    result = get_projections("2026", 1)
     with open(
         "/Users/jack/workspace/github.com/joc766/sleeper_wrangler/refs/temp.json", "w+"
     ) as f:
