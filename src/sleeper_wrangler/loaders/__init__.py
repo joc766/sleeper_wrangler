4569d35009d97fc32f0bc8cf6f855c851a364b69
Jack <razor-hautes01@icloud.com>
2026-09-24 23:28:41 -0400

    Add load_matchup_rosters to __init__ for loaders

--- src/sleeper_wrangler/loaders/__init__.py
+++ src/sleeper_wrangler/loaders/__init__.py
@@ -1,7 +1,7 @@
 from .draft import load_draft
 from .league import load_league
 from .matchup_players import load_matchup_players
-from .matchups import load_matchups
+from .matchups import load_matchup_rosters, load_matchups
 from .player_history import load_player_history
 from .players import load_players_data
 from .projections import load_projections
@@ -17,6 +17,7 @@ __all__ = [
     "load_league",
     "load_league_users",
     "load_matchup_players",
+    "load_matchup_rosters",
     "load_matchups",
     "load_player_history",
     "load_players_data",
