d1b6cb0a105e0fe1e53783e9b42a35af0b9c125f
Jack <razor-hautes01@icloud.com>
2026-09-24 23:25:09 -0400

    Add main function to espn api

--- src/sleeper_wrangler/espn_api.py
+++ src/sleeper_wrangler/espn_api.py
@@ -1,16 +1,18 @@
+import time
+
 import requests
 
 
 def get_game_statuses() -> dict[str, float]:
     """
     Returns dictionary of team abbreviations as keys
-    and float of percent complete as values
+    and float of percent complete as values.
     """
     abbr_corrections = {"WSH": "WAS"}
-    url = "https://site.api.espn.com/apis/fantasy/v2/games/ffl/games"
-    response = requests.get(url)
-    response.raise_for_status()
-    data = response.json()
+    url = "https://site.web.api.espn.com/apis/fantasy/v2/games/ffl/games"
+    r = requests.get(url)
+    r.raise_for_status()
+    data = r.json()
     completion_by_team: dict[str, float] = {}
     for event in data["events"]:
         for team in event["competitors"]:
@@ -20,3 +22,7 @@ def get_game_statuses() -> dict[str, float]:
             completion_by_team[abbr] = event["percentComplete"] / 100
 
     return completion_by_team
+
+
+if __name__ == "__main__":
+    print(get_game_statuses())
